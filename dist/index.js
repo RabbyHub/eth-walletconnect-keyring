63f90849dbb781539bce7f990b58788e34becd4f
heisenberg <heisenberg-2077@outlook.com>
2023-05-05 16:24:51 +0800

    build

--- dist/index.js
+++ dist/index.js
@@ -86,6 +86,7 @@ class WalletConnectKeyring extends events_1.EventEmitter {
                     }
                 }
                 this.updateCurrentStatus(exports.WALLETCONNECT_STATUS_MAP.FAILD, null, error || payload.params[0]);
+                this.onAfterConnect = null;
             };
             return connector;
         });
